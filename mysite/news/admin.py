7e6f07dfcb9eaddae1bdaa608461048b52e77aa8
Olga Shkola <shkola.olga@gmail.com>
2022-07-06 18:26:28 +0300

    cr

--- mysite/news/admin.py
+++ mysite/news/admin.py
@@ -3,7 +3,7 @@ from django.contrib import admin
 from .models import News
 
 class NewsAdmin(admin.ModelAdmin):
-    list_display = ('id', 'title', 'created_at', 'update_at', 'is_published')
+    list_display = ('id', 'title', 'cteated_at', 'update_at', 'is_published')
     list_display_links = ('id', 'title')
     search_fields = ('title', 'content')
 
--- mysite/news/models.py
+++ mysite/news/models.py
@@ -4,7 +4,7 @@ from django.db import models
 class News(models.Model):
     title = models.CharField(max_length=150, verbouse_name='Наименование')
     content = models.TextField(blank=True, verbouse_name='Контент')
-    created_at = models.DateTimeField(auto_now_add=True, verbouse_name='Дата публикации')
+    cteated_at = models.DateTimeField(auto_now_add=True, verbouse_name='Дата публикации')
     update_at = models.DateTimeField(auto_now=True, verbouse_name='Обновлено')
     photo = models.ImageField(upload_to='photos/%Y/%m/%d/', verbouse_name='Фото')
     is_published = models.BooleanField(default=True, verbouse_name='Опубликовано')
